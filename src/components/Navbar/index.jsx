a0e299a1c8a49a974e23a5d1aa9f1562df2b5096
MirShayan <shayan.ojaghi.mir@gmail.com>
2022-04-11 15:51:51 +0430

    Create Navbar Component

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,40 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./Navbar";
-const Index = () => {
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  StyledContainer,
+  NavLogo,
+  NavMenu,
+  NavItem,
+  MobileIcon,
+  NavLink,
+  NavBtn,
+  NavBtnLinkR,
+} from "./styledNavbar";
+import navMenuItems from "../../data/menuItems";
+const Navbar = ({ toggle }) => {
   return (
-    <Router>
-      <Navbar></Navbar>
-    </Router>
+    <Nav>
+      <StyledContainer>
+        <NavLogo to="/">MSONews</NavLogo>
+        <MobileIcon onClick={toggle}>
+          <FaBars />
+        </MobileIcon>
+        <NavMenu>
+          {navMenuItems.map(({ id, title, address }) => {
+            return (
+              <NavItem key={id}>
+                <NavLink to={address}>{title}</NavLink>
+              </NavItem>
+            );
+          })}
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLinkR to="/login">Login</NavBtnLinkR>
+        </NavBtn>
+      </StyledContainer>
+    </Nav>
   );
 };
 
-export default Index;
+export default Navbar;
---
+++ src/data/menuItems.js
@@ -0,0 +1,23 @@
+export const navMenuItems = [
+  {
+    id: 1,
+    title: "Home",
+    address: "/",
+  },
+  {
+    id: 2,
+    title: "Courses",
+    address: "/courses",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    address: "/blog",
+  },
+  {
+    id: 4,
+    title: "Road Map",
+    address: "/roadmap",
+  },
+];
+export default navMenuItems;
